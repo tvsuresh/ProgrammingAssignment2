3a63da4784c8369fdcd3ad08d5ec2d1dbb07885f
tvsuresh <sthiruka@STHIRUKA-WS02.cisco.com>
2015-07-26 13:48:27 -0700

    Assignment Wk 3
    Added code for makeCacheMateix and cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,15 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  sv <- NULL
+  set <- function(y) {
+    x <<- y
+    sv <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(svx) sv <<- svx
+  getsolve <- function() sv
+  list(set = set, get = get, setsolve = setsolve, getsolve = getsolve)
 }
 
 
@@ -12,4 +20,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
 }
